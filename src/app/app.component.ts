8557d6be4f9c952785fa4419b55c8bfccd67e94c
talfumier <henri.talfumier@gmail.com>
2024-01-10 17:05:01 +0100

    Minor changes in home.component.html and app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
-import { Component, OnInit} from '@angular/core';
+import { Component,OnInit} from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  
+export class AppComponent implements OnInit{
+  constructor(private router:Router) {}
+  ngOnInit(): void {
+    this.router.navigate(['']);//Make sure home page is displayed after page reload
+  }
 }
